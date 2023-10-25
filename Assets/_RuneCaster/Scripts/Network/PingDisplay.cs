e71b546e0f193f8610e396b84c4d642b046644cf
bryant-ta <33585269+bryant-ta@users.noreply.github.com>
2023-10-24 17:30:54 -0700

    added simple ping display

---
+++ Assets/_RuneCaster/Scripts/Network/PingDisplay.cs
@@ -0,0 +1,25 @@
+using System;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+
+// TODO: replace with colored icon (green, yellow, red) or a tab ping screen
+public class PingDisplay : MonoBehaviour {
+	TextMeshProUGUI _pingText;
+
+	void Awake() {
+		_pingText = GetComponent<TextMeshProUGUI>();
+	}
+
+	float _timer = int.MaxValue;
+	float _pingUpdateInterval = 5f;
+	void Update() {
+		if (_timer > _pingUpdateInterval) {
+			_timer = 0;
+			_pingText.text = PhotonNetwork.NetworkingClient.LoadBalancingPeer.RoundTripTime.ToString();
+		}
+
+		_timer += Time.deltaTime;
+	}
+}
